dc05e993bc887ed6274e32323e200fcb0f47630c
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-07-06 23:50:21 +0530

    Ui update

--- src/components/home/Faq.jsx
+++ src/components/home/Faq.jsx
@@ -5,6 +5,15 @@ function Faq() {
   return (
     <>
       <div className="w-full py-16 bg-slate-50">
+        <div className="absolute w-[600px] h-auto top-[-16rem] right-0">
+          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill="#ffff"
+              d="M50.9,-45.3C64.5,-37.4,72.8,-18.7,72.9,0.1C73,18.9,64.9,37.9,51.4,53.7C37.9,69.5,18.9,82.1,2.6,79.5C-13.7,76.8,-27.3,58.9,-36.6,43.1C-45.8,27.3,-50.7,13.7,-48.2,2.5C-45.7,-8.7,-35.9,-17.4,-26.6,-25.3C-17.4,-33.2,-8.7,-40.4,5,-45.4C18.7,-50.4,37.4,-53.3,50.9,-45.3Z"
+              transform="translate(100 100)"
+            />
+          </svg>
+        </div>
         <Container>
           <h2 className="font-bold ">Frequently Asked Questions</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
--- src/components/home/Join.jsx
+++ src/components/home/Join.jsx
@@ -4,7 +4,7 @@ import Container from "../layout/Container";
 function Join() {
   return (
     <>
-      <div className="py-6 bg-slate-100">
+      <div className="py-6 bg-slate-50">
         <div className="w-full absolute -bottom-1 sm:-bottom-8 left-0">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
@@ -18,8 +18,17 @@ function Join() {
         <Container>
           <div
             className=" bg-green-300 rounded-xl mx-auto h-auto sm:h-[250px]
-            flex items-center justify-start sm:justify-center"
+            flex items-center justify-start sm:justify-center overflow-hidden"
           >
+            <div className="absolute w-[260px] h-auto bottom-[-4rem] left-[-5rem]">
+              <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="#5ddb8a"
+                  d="M68.3,-37C82.6,-14.6,84.2,17.5,70.7,38.3C57.2,59.2,28.6,68.8,2,67.7C-24.7,66.6,-49.4,54.7,-58.9,36.1C-68.4,17.5,-62.8,-7.7,-50.4,-29C-38.1,-50.3,-19,-67.6,4,-69.9C27,-72.2,53.9,-59.5,68.3,-37Z"
+                  transform="translate(100 100)"
+                />
+              </svg>
+            </div>
             <div className="p-8 sm:p-16 text-left sm:text-center">
               <p className=" mb-2">Join over 500,000 students</p>
               <h2 className="mb-4 max-w-lg font-medium">
@@ -31,6 +40,16 @@ function Join() {
                   Get started
                 </a>
               </Link>
+            </div>
+
+            <div className="absolute w-[260px] h-auto top-[-4rem] right-[-5rem]">
+              <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="#5ddb8a"
+                  d="M57.5,-35.2C68.8,-13.6,68.4,12.8,56.8,26.5C45.3,40.2,22.6,41.1,-0.5,41.4C-23.7,41.7,-47.4,41.4,-61.4,26.3C-75.4,11.3,-79.6,-18.6,-67.7,-40.5C-55.8,-62.4,-27.9,-76.5,-2.4,-75.1C23.1,-73.7,46.2,-56.8,57.5,-35.2Z"
+                  transform="translate(100 100)"
+                />
+              </svg>
             </div>
           </div>
         </Container>
